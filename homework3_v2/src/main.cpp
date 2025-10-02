64053d5ec5fa72b1c6b2eef6007ad614dbbd5c9b
Haruki152 <528763389@qq.com>
2025-10-03 06:42:20 +0800

    Update main.cpp

--- homework3_v2/src/main.cpp
+++ homework3_v2/src/main.cpp
@@ -281,11 +281,11 @@ int main() {
     // 参数边界约束
     problem.AddParameterBlock(initial_parameters, 4,nullptr);
     
-    // // 设置参数边界: g ∈ [100, 1000], k ∈ [0.01, 1]
-    // ceres::SubsetParameterization* parameterization = 
-    //     new ceres::SubsetParameterization(4, {2, 3});  // 只对g和k设置边界
+     // 设置参数边界: g ∈ [100, 1000], k ∈ [0.01, 1]
+     ceres::SubsetParameterization* parameterization = 
+         new ceres::SubsetParameterization(4, {2, 3});  // 只对g和k设置边界
     
-    // problem.SetParameterization(initial_parameters, parameterization);
+     problem.SetParameterization(initial_parameters, parameterization);
     
     // 添加边界约束
     problem.SetParameterLowerBound(initial_parameters, 2, 100.0);   // g下限
