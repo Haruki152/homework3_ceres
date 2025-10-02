6da2ab7b6dbbe69e588120d3bbd464cc5a0fd9a7
Haruki152 <528763389@qq.com>
2025-10-03 06:36:34 +0800

    Add files via upload

---
+++ homework3_v2/build/CMakeFiles/video_reader.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/video_reader.dir/src/main.cpp.o"
+  "CMakeFiles/video_reader.dir/src/main.cpp.o.d"
+  "bin/video_reader"
+  "bin/video_reader.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/video_reader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
